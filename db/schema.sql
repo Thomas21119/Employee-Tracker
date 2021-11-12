2d3063bc0f6efb9bccebebe50880b4d6c59a0298
Thomas21119 <thomas.wittwer2018@gmail.com>
2021-11-13 10:57:41 +1100

    attempted fix not working

--- db/schema.sql
+++ db/schema.sql
@@ -3,26 +3,26 @@ CREATE DATABASE employee_db;
 
 USE employee_db;
 
-CREATE TABLE all_departments (
+CREATE TABLE IF NOT EXISTS all_departments (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   department_name VARCHAR(100) NOT NULL
 );
 
-CREATE TABLE all_roles (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    job_title VARCHAR(100) NOT NULL,
-    department_id INT NOT NULL,
-    salary DECIMAL NOT NULL,
-    FOREIGN KEY (department_id) REFERENCES all_departments(id)
+CREATE TABLE IF NOT EXISTS all_roles (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  job_title VARCHAR(100) NOT NULL,
+  department_id INT NOT NULL,
+  salary DECIMAL NOT NULL,
+  FOREIGN KEY (department_id) REFERENCES all_departments(id)
 );
 
-CREATE TABLE all_employees (
-    employee_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    employee_first VARCHAR(100) NOT NULL,
-    employee_last VARCHAR(100) NOT NULL,
-    role_id INT NOT NULL,
-    manager_id INT NOT NULL,
-    FOREIGN KEY (role_id) REFERENCES all_roles(id),
-    FOREIGN KEY (manager_id) REFERENCES all_employees(employee_id),
+CREATE TABLE IF NOT EXISTS yeet (
+  employee_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  employee_first VARCHAR(100) NOT NULL,
+  employee_last VARCHAR(100) NOT NULL,
+  role_id INT NOT NULL,
+  manager_id INT NOT NULL,
+  FOREIGN KEY (role_id) REFERENCES all_roles(id),
+  FOREIGN KEY (manager_id) REFERENCES yeet(employee_id),
 );
 
